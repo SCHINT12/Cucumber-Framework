1d3a8a45421b68a2c5d7f8425a045b724331749b
SCHINT12 <156803682+SCHINT12@users.noreply.github.com>
2024-11-24 00:45:35 +0530

    Update TestNGTestRunner.java

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -5,7 +5,7 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/test/java/features",glue ="stepDefinitions", tags = "@PlaceOrder",
+@CucumberOptions(features="src/test/java/features",glue ="stepDefinitions", tags = "@PlaceOrder"
 ,monochrome=true,
 plugin= {"html:target/cucumber.html", "json:target/cucumber.json",
 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
